e29128fb2ce745ae0785546e4d959e8d79b9e50d
vladleeeeeeen <inechitack229@gmail.com>
2025-02-14 23:51:42 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -70,7 +70,7 @@ function App() {
         <div className="main">
             {isValentine ? (
                 <>
-                    <h2 className="title"> I love you &hearts; </h2>
+                    <h2 className="title"> To the best and most beautiful girl &hearts; </h2>
                     <img alt="cat animation" src={catAnimation} />
                 </>
             ) : (
